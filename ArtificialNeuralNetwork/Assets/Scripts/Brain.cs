9e542f85d3c432f715cca426354abfe8b55e7831
anant sharma <anant@rvtechnologies.com>
2019-08-23 17:05:44 +0530

    ann added

---
+++ ArtificialNeuralNetwork/Assets/Scripts/Brain.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    /// <summary>
+    /// Our artificial neural network
+    /// </summary>
+    ANN ann;
+
+    /// <summary>
+    /// A value we use in Stats to calculate how close the model
+    /// we have constructed fits the data we fed into it
+    /// </summary>
+    /// <remarks>
+    /// The closer it is to 0 the better trained model it is
+    /// </remarks>
+    double sumSquareError = 0;
+
+
+    private void Start()
+    {
+        //make an ann
+        //value of alpha is from 0-1
+        ann = new ANN(2, 1, 1, 2, 0.8);
+
+        List<double> result;
+
+        //epochs
+        for (int i = 0; i < 100000; i++)
+        {
+            sumSquareError = 0;
+
+            //XNOR
+            result = Train(1, 1, 1);
+
+            // (the result we get back - the desired result)^2
+            sumSquareError += Mathf.Pow((float) result[0] - 1,2);
+
+            result = Train(1, 0, 0);
+            sumSquareError += Mathf.Pow((float)result[0] - 0, 2);
+
+            result = Train(0, 1, 0);
+            sumSquareError += Mathf.Pow((float)result[0] - 0, 2);
+
+            result = Train(0, 0, 1);
+            sumSquareError += Mathf.Pow((float)result[0] - 1, 2);
+        }
+        Debug.Log("SSE: " + sumSquareError);
+
+        result = Train(1, 1, 1);
+        Debug.Log(" 1,1 = " + result[0]);
+
+        result = Train(1, 0, 0);
+        Debug.Log(" 1,0 = " + result[0]);
+
+        result = Train(0, 1, 0);
+        Debug.Log(" 0,1 = " + result[0]);
+
+        result = Train(0, 0, 1);
+        Debug.Log(" 0,0 = " + result[0]);
+    }
+
+
+    List<double> Train(int _input1, int _input2, int _output)
+    {
+        List<double> inputs = new List<double>();
+        List<double> outputs = new List<double>();
+
+        inputs.Add(_input1);
+        inputs.Add(_input2);
+        outputs.Add(_output);
+
+
+        return (ann.Calculate(inputs,outputs));
+    }
+}
---
+++ ArtificialNeuralNetwork/Assets/Scripts/Neuron.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A neuron is basically a perceptron
+/// </summary>
+public class Neuron
+{
+    /// <summary>
+    /// It's the number of Inputs,
+    /// usually a neuron is connected to everyother neuron of the previous layer
+    /// </summary>
+    /// <remarks>
+    /// There could be alot of inputs coming into it
+    /// </remarks>
+    public int numInputs;
+
+    /// <summary>
+    /// Extra bias value to move around the results
+    /// </summary>
+    public double bias;
+
+    /// <summary>
+    /// The output from the nueron same as the output from the perceptron
+    /// </summary>
+    public double output;
+
+    /// <summary>
+    /// Assigning the amont of error to that neuron and it's weight
+    /// </summary>
+    public double errorGradient;
+
+    /// <summary>
+    /// List of all of the weights
+    /// </summary>
+    public List<double> weights = new List<double>();
+
+    /// <summary>
+    /// List of all of the inputs from the previous layer
+    /// </summary>
+    /// <remarks> 
+    /// It can come from outside the Neural network or from the previous layer
+    /// </remarks>
+    public List<double> inputs = new List<double>();
+
+    public Neuron(int nInputs)
+    {
+        //Settings the bias to random number
+        bias = Random.Range(-1f, 1f);
+
+        numInputs = nInputs;
+
+        //Setting the weights randomly
+        for (int i = 0; i < nInputs; i++)
+        {
+            weights.Add(Random.Range(-1f, 1f));
+        }
+    }
+}
